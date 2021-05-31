c757be13aa36b50ef21fb5e86be8d9e66afb8635
Ramiro <ramegp@ramegp>
2021-05-31 15:39:45 -0300

    agrego homepage e imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Menu from './components/Menu/Menu';
+import Home from './components/Home/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -8,6 +9,8 @@ function App() {
   return (
     <div className="App">
       <Menu/>
+      <Home/>
+
     </div>
   );
 }
