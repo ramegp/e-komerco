97e592792c1e49085702c7244a9f7e6096224187
Ramiro <ramegp@ramegp>
2021-06-08 20:35:39 -0300

    add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,17 @@ export default function Home(){
         <>
             <div className="container-fluid container-home" id="home">
 
+                <a href="https://wa.me/5492215638168/?text=tu%20texto%20personalizado.%20Texto%20Creado%0A%20manual%0
+                %20Enter%20
+                12%2020asdadasdas asd ad asda da sda dasd kawdfkl aerjgnserjgjksengkjerngjkwrengjnqwjnfwer 
+                qwelrkwmltkwerlk mlekmy klewmtykmektmwtmwqñtmñwr
+                a
+                aasdasd
+                asda" className="btn-whatsapp">Anchor text</a>
+                <a href="https://wa.me/5492215638168?text=%28ukemochi%29+%0A+Hola+como+estas%3F+%21+%3E" className="btn-whatsapp">Send Message</a>
+
+                <a>link</a>
+
             </div>
         </>
     )
