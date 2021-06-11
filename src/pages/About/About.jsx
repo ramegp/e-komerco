c505169979df12fb580c9bf5c401a6f722874284
Ramiro <ramegp@ramegp>
2021-06-10 23:31:25 -0300

    leo la api pokemon

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './About.css'
-
+import Poke from '../../components/FetchPokemon/FetchPokemon'
 function About() {
     return (
         <div className="container-about" id="about">
-            
+            <Poke />
         </div>
     )
 }
