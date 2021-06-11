281207ca878e8d8cc59f420e11d17f872594eef1
Ramiro <ramegp@ramegp>
2021-06-10 23:30:46 -0300

    Leo productos de una api

--- src/components/ShowProducts/ShowProducts.jsx
+++ src/components/ShowProducts/ShowProducts.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../../components/Cards/CardsItem";
 
 /* Import productos */
-import productos from "../../products.json";
-localStorage.setItem("productos", JSON.stringify(productos))
+/* import productos from "../../products.json";
+localStorage.setItem("productos", JSON.stringify(productos)); */
 
+function ShowProducts(props) {
+  const [productos, setData] = useState(null);
 
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      });
+  }, []);
 
-
-function ShowProducts() {
   return (
     <div className="container-fluid d-flex flex-wrap">
-      {productos.map((prod,index) => {
+
+      {productos !== null ? (productos.map((prod, index) => {
         return (
           <>
             <Card
-              name={prod.name}
+              name={prod.title}
               description={prod.description}
               category={prod.category}
               price={prod.price}
               id={index}
+              img={prod.image}
+              function={props.function}
             />
           </>
         );
-      })}
+      })) :(<h2>Cargando datos</h2>)}
+
+      
+
     </div>
   );
 }
